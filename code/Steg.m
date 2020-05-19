24c15fa19d2acf43b79f97c961590f7d827d488f
NeiNeken <54332656+NeiNeken@users.noreply.github.com>
2020-05-19 20:03:51 +0300

    Add files via upload

--- code/Steg.m
+++ code/Steg.m
@@ -14,40 +14,52 @@
         end
     end
     sample_uint16=uint16(sample_int_abs);
-    encKey=file2bin('encryptionKey.txt')';
-    %encKey=encKey1';
+    encKey1=file2bin('encryptionKey.txt')';
+    encKey=encKey1';
     
     s1=file2bin('msg.txt');
     s=s1';
     nsample_uint16=sample_uint16;
     %LSB leri s1 deki bitlere uygun mu diye kontrol et
-    L=numel(s);
+    L=length(Y);
+    L1=numel(s);
+    encLength=de2bi(L1, 17)';
+    encLengthChar= num2str(encLength);
     
     crypLength=length(Y)-100;
-    bitSpace1=floor(crypLength/length(s1));
-    bitSpace=de2bi(bitSpace1, 12)';
-    bitSpace2=bitSpace(:)';
-    bitSpChar= char(bitSpace);
+    bitSpace=floor(crypLength/length(s1));
+    bitSpace1=de2bi(bitSpace, 12)';
+    bitNumtostr= num2str(bitSpace1);
+    finalEnchChar = vertcat(bitNumtostr, encLengthChar);
     for i=1:88 % 8 character password ü gömme
         
-        [bit,encKey]=getBits(encKey,1);
-        nsample_uint16(i) = LSB(sample_uint16(i), bit, 1);
+        [bit,encKey1]=getBits(encKey1);
+        nsample_uint16(i) = LSB(sample_uint16(i), bit);
     end
-    
+    lul=88+numel(finalEnchChar);
     k=0;
-    for i=89:100
-      disp(bitSpChar(i-88));
-        %  nsample_uint16(i) = LSB(sample_uint16(i),bitSpace(i-88),1);
-       nsample_uint16(i) = bitSpace(i-88);
+    for i=89:lul-1
+      disp((i));
+         [bit, finalEnchChar] = getBits(finalEnchChar);
+         disp(bit);
+         nsample_uint16(i) = LSB(sample_uint16(i),finalEnchChar(1));
+      %nsample_uint16(i) = bitSpace(i-88);
     end
-    
-  for i=100:L
-   % disp(i);
+    spaceCounter= 0;
+    stegCount= 0;
+  for i=lul:L
+      if(i<1000)
+    disp(i);
+      end
+            if( spaceCounter == 0 && numel(s) ~= 0)
+             disp('gömdük');
+             [bit,s]=getBits(s);
+            nsample_uint16(i)=LSB(sample_uint16(i),bit);
+            spaceCounter = bitSpace;
+            stegCount = stegCount +1;
             
-            [bit,s]=getBits(s,1);
-            nsample_uint16(i)=LSB(sample_uint16(i),bit,1);
-           
-      
+            end
+        spaceCounter = spaceCounter -1;
   end
     %% Display stego Samples
     
@@ -78,16 +90,16 @@ binn=bin(:);
 fclose(FID);
  end   
 
- function [sample] = LSB(sample,bits,length)
+ function [sample] = LSB(sample,bit)
 %LSB4 Summary of this function goes here
 %   Detailed explanation goes here
-sample=bitshift(sample,-length);
-sample=bitshift(sample,length);
-bitval=bin2dec(bits);
+sample=bitshift(sample,-1);
+sample=bitshift(sample,1);
+bitval=bin2dec(bit);
 sample=sample+bitval;
 end
 
-function [token,remaining] = getBits(bits,tokenLength)
-token=bits(1,1:tokenLength);
-remaining=bits(1,tokenLength+1:end);
+function [token,remaining] = getBits(bits)
+token=bits(1);
+remaining=bits(2:end);
 end
